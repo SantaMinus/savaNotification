1aa761f3190210b88d3cf7521b6aa12b93e7064a
ksavina <kateryna_savina_tp@bmc.com>
2018-12-18 16:40:59 +0200

    Start implementing MyBatis data access

--- src/main/java/com/sava/savaNotification/dao/SubscriptionDaoImpl.java
+++ src/main/java/com/sava/savaNotification/dao/SubscriptionDaoImpl.java
@@ -6,6 +6,9 @@ import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sava.savaNotification.mapper.SubscriptionMapper;
 
 import nl.martijndwars.webpush.Subscription;
 
@@ -16,15 +19,23 @@ import nl.martijndwars.webpush.Subscription;
 public class SubscriptionDaoImpl implements SubscriptionDao {
     private Logger LOGGER = LoggerFactory.getLogger(SubscriptionDaoImpl.class);
 
+    private final SubscriptionMapper mapper;
+
+    public SubscriptionDaoImpl(SubscriptionMapper mapper) {
+        this.mapper = mapper;
+    }
+
     @Override
     public List<Subscription> getAll() {
         return null;
     }
 
     @Override
+    @Transactional
     public void setSubscription(Subscription sub) {
         Validate.notNull(sub, "The subscription can't be null");
 
         LOGGER.debug("Setting a subscription..");
+        mapper.create(sub.endpoint, sub.keys.auth, sub.keys.p256dh);
     }
 }
---
+++ src/main/java/com/sava/savaNotification/mapper/CommonSql.java
@@ -0,0 +1,11 @@
+package com.sava.savaNotification.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Class used only for initialization by mybatis of xml in resource with the same name,
+ * which contain code for retrieving of current sequence number
+ */
+@Mapper
+public interface CommonSql {
+}
