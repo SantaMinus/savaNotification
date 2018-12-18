752e4f74d15d0da792b263be733e189fc5bcb4bc
ksavina <kateryna_savina_tp@bmc.com>
2018-12-18 11:12:40 +0200

    Initial commit

---
+++ src/test/java/com/sava/savaNotification/SavaNotificationApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sava.savaNotification;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SavaNotificationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
