66596606eab676603b8dfce973fe1510ffcd93ff
SantaMinus <santaminus@gmail.com>
2019-07-12 22:44:41 +0300

    Add @CrossOrigin annotation to resources

--- src/main/java/com/sava/savaNotification/rest/NotificationResource.java
+++ src/main/java/com/sava/savaNotification/rest/NotificationResource.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import com.sava.savaNotification.service.NotificationService;
 /**
  * @author kateryna.savina
  */
+@CrossOrigin
 @RestController
 @RequestMapping("/notification")
 public class NotificationResource {
--- src/main/java/com/sava/savaNotification/rest/SubscriptionResource.java
+++ src/main/java/com/sava/savaNotification/rest/SubscriptionResource.java
@@ -4,22 +4,17 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.sava.savaNotification.fcm.FcmClient;
 import com.sava.savaNotification.service.SubscriptionService;
 
 import nl.martijndwars.webpush.Subscription;
 
-
 /**
  * @author kateryna.savina
  */
+@CrossOrigin
 @RestController
 @RequestMapping("/subscription")
 public class SubscriptionResource {
