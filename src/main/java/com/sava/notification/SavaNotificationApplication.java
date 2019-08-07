bada4e17be5e1d67714430a9a57bed24cf6c525e
SantaMinus <santaminus@gmail.com>
2019-08-07 22:11:15 +0300

    Remove unused LOGGER

--- src/main/java/com/sava/notification/SavaNotificationApplication.java
+++ src/main/java/com/sava/notification/SavaNotificationApplication.java
@@ -1,14 +1,10 @@
 package com.sava.notification;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SavaNotificationApplication {
-
-    public static final Logger LOGGER = LoggerFactory.getLogger(SavaNotificationApplication.class);
 
     public static void main(String[] args) {
 		SpringApplication.run(SavaNotificationApplication.class, args);
